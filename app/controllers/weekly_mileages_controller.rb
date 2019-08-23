2fe63d9353143c41963569b5ac9d07d6a5823a80
Raghu Betina <raghu@firstdraft.com>
2019-08-23 20:55:04 +0000

    Only weekly_mileage user can modify weekly_mileage

--- app/controllers/weekly_mileages_controller.rb
+++ app/controllers/weekly_mileages_controller.rb
@@ -1,4 +1,14 @@
 class WeeklyMileagesController < ApplicationController
+  before_action :current_user_must_be_weekly_mileage_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_weekly_mileage_user
+    weekly_mileage = WeeklyMileage.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == weekly_mileage.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @weekly_mileages = WeeklyMileage.all
 
