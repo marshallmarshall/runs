8ac56c51edaf9d89df3e38910814bf277e7f4eb9
Raghu Betina <raghu@firstdraft.com>
2019-08-23 20:55:04 +0000

    Only shoe user can modify shoe

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,4 +1,14 @@
 class ShoesController < ApplicationController
+  before_action :current_user_must_be_shoe_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_shoe_user
+    shoe = Shoe.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == shoe.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @shoes = Shoe.all
 
