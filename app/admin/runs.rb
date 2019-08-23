096099a21436814c3e0148e4c893650b9bb74121
Raghu Betina <raghu@firstdraft.com>
2019-08-23 20:54:33 +0000

    Whitelist columns in Run for ActiveAdmin

--- app/admin/runs.rb
+++ app/admin/runs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Run do
+
+ permit_params :date_of_run, :training_week, :miles_planned, :miles_actual, :treadmill_incline, :run_duration_minutes, :run_duration_seconds, :actual_pace, :location_id, :notes, :user_id, :shoe_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
