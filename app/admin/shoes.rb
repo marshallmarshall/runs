179eec7fd5f3882bfc2da34312d44d0fbc42914f
Raghu Betina <raghu@firstdraft.com>
2019-08-23 20:54:49 +0000

    Whitelist columns in Shoe for ActiveAdmin

--- app/admin/shoes.rb
+++ app/admin/shoes.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Shoe do
+
+ permit_params :shoe_name, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
