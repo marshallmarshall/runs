740a2843afbe0e6626844d29ca58d67857ed20bf
Raghu Betina <raghu@firstdraft.com>
2019-08-23 20:54:59 +0000

    Whitelist columns in MonthlyMileage for ActiveAdmin

--- app/admin/monthly_mileages.rb
+++ app/admin/monthly_mileages.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register MonthlyMileage do
+
+ permit_params :month_starting, :monthly_mileage, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
