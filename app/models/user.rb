570adbd43e558331da709de16b050ca0bb943ac2
Raghu Betina <raghu@firstdraft.com>
2019-08-23 20:54:12 +0000

    Direct association between users and locations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :locations,
+             :dependent => :destroy
+
   has_many   :runs,
              :dependent => :nullify
 
