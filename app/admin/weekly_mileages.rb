cba9698a2f1cbecbbd846ef3c8b783b4bd55b993
Raghu Betina <raghu@firstdraft.com>
2019-08-23 20:54:54 +0000

    Whitelist columns in WeeklyMileage for ActiveAdmin

--- app/admin/weekly_mileages.rb
+++ app/admin/weekly_mileages.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register WeeklyMileage do
+
+ permit_params :week_starting, :weekly_mileage, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
