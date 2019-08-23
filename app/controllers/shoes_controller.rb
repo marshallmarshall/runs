af3a5348c68aeff8f0fc2b04dfafa1b05afd91ce
Raghu Betina <raghu@firstdraft.com>
2019-08-23 20:55:04 +0000

    Only shoe user can access show page

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,5 +1,5 @@
 class ShoesController < ApplicationController
-  before_action :current_user_must_be_shoe_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_shoe_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_shoe_user
     shoe = Shoe.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
