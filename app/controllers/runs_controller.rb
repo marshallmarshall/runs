19bf031596c41da034bfb92f6f32d50d6b371321
Raghu Betina <raghu@firstdraft.com>
2019-08-23 20:55:04 +0000

    Only run user can access show page

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,5 +1,5 @@
 class RunsController < ApplicationController
-  before_action :current_user_must_be_run_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_run_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_run_user
     run = Run.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
